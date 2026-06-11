69994996c116a8ac12be7798d6a2056a13123f0c
MatiasIMunoz <ma.munozsandoval@gmail.com>
2026-06-11 09:48:25 -0500

    Added sessionInfo() at the end of the script.

--- Nice_oscillo_spectro.R
+++ Nice_oscillo_spectro.R
@@ -4,7 +4,7 @@
 # ___________________________
 #
 # 0) Load the modified 'prep_static_ggspectro' function to create spectrograms of stereo files. ----
-#
+# This .R script should be stored in the working directory for this to work.
 #___________________________
 source("modif_spectro_function.R")
 
@@ -232,3 +232,39 @@ plot_grid(oscillo_LR, spectro_LR,
           axis    = "tb",
           ncol = 2,
           rel_heights = c(1, 1))
+
+
+# END OF SCRIPT ----
+
+ 
+# > sessionInfo()
+# R version 4.3.1 (2023-06-16)
+# Platform: x86_64-apple-darwin20 (64-bit)
+# Running under: macOS 15.4.1
+# 
+# Matrix products: default
+# BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.3-x86_64/Resources/lib/libRlapack.dylib;  LAPACK version 3.11.0
+# 
+# locale:
+#   [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+# 
+# time zone: America/Chicago
+# tzcode source: internal
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] Cairo_1.6-2       extrafont_0.19    ggpubr_0.6.0      cowplot_1.1.1     ggplot2_3.5.2     poisonfrogs_1.0.2 viridis_0.6.3    
+# [8] viridisLite_0.4.2 tuneR_1.4.7       seewave_2.2.3     dynaSpec_1.0.1   
+# 
+# loaded via a namespace (and not attached):
+#   [1] generics_0.1.4     tidyr_1.3.0        rstatix_0.7.2      extrafontdb_1.0    digest_0.6.37      magrittr_2.0.4     evaluate_1.0.3    
+# [8] grid_4.3.1         RColorBrewer_1.1-3 pkgload_1.4.0      fastmap_1.2.0      backports_1.5.0    gridExtra_2.3      purrr_1.0.2       
+# [15] scales_1.4.0       textshaping_0.3.6  isoband_0.2.7      abind_1.4-5        cli_3.6.5          rlang_1.1.6        crayon_1.5.3      
+# [22] yaml_2.3.10        withr_3.0.2        tools_4.3.1        ggsignif_0.6.4     dplyr_1.1.4        broom_1.0.5        vctrs_0.6.5       
+# [29] R6_2.5.1           lifecycle_1.0.4    car_3.1-2          MASS_7.3-60        ragg_1.2.5         pkgconfig_2.0.3    pillar_1.10.1     
+# [36] gtable_0.3.6       rsconnect_1.0.1    glue_1.8.0         systemfonts_1.2.3  xfun_0.50          tibble_3.2.1       tidyselect_1.2.0  
+# [43] knitr_1.49         rstudioapi_0.15.0  dichromat_2.0-0.1  farver_2.1.2       htmltools_0.5.8.1  rmarkdown_2.29     carData_3.0-5     
+# [50] labeling_0.4.3     Rttf2pt1_1.3.12    signal_1.8-1       compiler_4.3.1    
