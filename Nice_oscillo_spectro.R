d3ae20bf981193b8e8c04ac9c6c816156c172ad3
MatiasIMunoz <ma.munozsandoval@gmail.com>
2026-06-11 11:19:55 -0500

    Commented image saving code

--- Nice_oscillo_spectro.R
+++ Nice_oscillo_spectro.R
@@ -123,7 +123,7 @@ oscillo_LR
 
 # Export figure
 #ggsave("Hyla_stereo_oscillo.pdf", plot = oscillo_LR, device = cairo_pdf, width = 9, height = 4)
-ggsave("Hyla_stereo_oscillo.png", plot = oscillo_LR, width = 9, height = 4, units = "in", dpi = 600)
+#ggsave("Hyla_stereo_oscillo.png", plot = oscillo_LR, width = 9, height = 4, units = "in", dpi = 600)
 
 #___________________________
 #
@@ -225,7 +225,7 @@ spectro_LR
 
 # Export figure
 #ggsave("Hyla_stereo_spectro.pdf", plot = spectro_LR, device = cairo_pdf, width = 9, height = 6)
-ggsave("Hyla_stereo_spectro.png", plot = spectro_LR, width = 9, height = 6, units = "in", dpi = 600)
+#ggsave("Hyla_stereo_spectro.png", plot = spectro_LR, width = 9, height = 6, units = "in", dpi = 600)
 
 #___________________________
 #
